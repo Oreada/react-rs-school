2f8d5f62be0be8172a838c9650c3542f58d6d030
Olga Antushevich <antushevich@gmail.com>
2022-09-30 10:51:13 +0300

    feat: add ReactNode to class

--- react-components/src/components/Navigation.tsx
+++ react-components/src/components/Navigation.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 export class Navigation extends React.Component {
-	render() {
+	render(): ReactNode {
 		return (
 			<nav className="h-[50px] flex justify-between items-center px-5 bg-lime-900 text-white">
-				<span className="font-bold">REACT 2022</span>
+				<span className="font-bold">RS-SCHOOL REACT</span>
 				<span>
 					<Link to="/" className="mr-5">Home</Link>
 					<Link to="/about">About</Link>
