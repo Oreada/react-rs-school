83ded37235e90d78eaa1641ae50eeb5efc3a308d
Olga Antushevich <antushevich@gmail.com>
2022-10-03 11:54:02 +0300

    feat: add list of products, add some style

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
-import { Product } from './components/Product';
 import { AboutPage } from './pages/AboutPage';
 import { ErrorPage } from './pages/ErrorPage';
 import { HomePage } from './pages/HomePage';
 
 function App() {
     return (
-        <>
+        <div className="container">
             <Navigation />
             <Routes>
                 <Route path='/' element={<HomePage />} />
@@ -16,8 +15,7 @@ function App() {
                 <Route path='/404' element={<ErrorPage />} />
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
-            <Product image={''} name={''} brand={''} volume={0} color={''} weight={0} price={0} popular={''} storage={0} />
-        </>
+        </div>
     );
 }
 
