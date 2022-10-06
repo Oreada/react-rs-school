eb9e27cf8aa74e4509e2511220083c7838aa58e9
Olga Antushevich <antushevich@gmail.com>
2022-10-06 11:57:12 +0300

    feat: add more forms

--- react-components/src/components/Form.tsx
+++ react-components/src/components/Form.tsx
@@ -2,32 +2,46 @@ import React from 'react';
 import { ReactNode } from 'react';
 
 interface FormState {
-  inputText: string;
+  inputNameText: string;
+  inputPhoneText: string;
   textareaText: string;
   selectText: string;
+  checkboxText: string;
   showData: {
     name: string;
-    text: string;
-    position: string;
+    phone: string;
+    adress: string;
+    delivery: string;
+    payment: string;
   };
 }
 
 export class Form extends React.Component<Record<string, never>, FormState> {
   state = {
-    inputText: '',
+    inputNameText: '',
+    inputPhoneText: '',
     textareaText: '',
-    // selectText: 'Front-end developer',
+    // selectText: 'Self-delivery',
     selectText: '',
+    checkboxText: 'cash',
     showData: {
       name: '',
-      text: '',
-      position: '',
+      phone: '',
+      adress: '',
+      delivery: '',
+      payment: '',
     },
   };
 
-  handleInputChange = ({ target: { value } }: { target: { value: string } }) => {
+  handleInputNameChange = ({ target: { value } }: { target: { value: string } }) => {
     this.setState({
-      inputText: value,
+      inputNameText: value,
+    });
+  };
+
+  handleInputPhoneChange = ({ target: { value } }: { target: { value: string } }) => {
+    this.setState({
+      inputPhoneText: value,
     });
   };
 
@@ -43,16 +57,32 @@ export class Form extends React.Component<Record<string, never>, FormState> {
     });
   };
 
-  handleShow = (event: React.MouseEvent) => {
+  handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(event.target.checked);
+    let payment = '';
+    if (event.target.checked) {
+      payment = 'card';
+    } else {
+      payment = 'cash';
+    }
+
+    this.setState({
+      checkboxText: payment,
+    });
+  };
+
+  handleSubmit = (event: React.MouseEvent) => {
     event.preventDefault();
     this.setState({
       // inputText: '',
       // textareaText: '',
       // selectText: '',
       showData: {
-        name: this.state.inputText,
-        text: this.state.textareaText,
-        position: this.state.selectText,
+        name: this.state.inputNameText,
+        phone: this.state.inputPhoneText,
+        adress: this.state.textareaText,
+        delivery: this.state.selectText,
+        payment: this.state.checkboxText,
       },
     });
   };
@@ -61,21 +91,33 @@ export class Form extends React.Component<Record<string, never>, FormState> {
     return (
       <>
         <form className="form">
-          {/* input text */}
-          <label className="form-label-input" htmlFor="form-input">
+          {/* input: NAME */}
+          <label className="form-label-input" htmlFor="form-input-name">
             Name:
           </label>
           <input
             className="form-input"
             type="text"
             name="name"
-            id="form-input"
-            value={this.state.inputText}
-            onChange={this.handleInputChange}
+            id="form-input-name"
+            value={this.state.inputNameText}
+            onChange={this.handleInputNameChange}
           />
-          {/* textarea */}
+          {/* input: PHONE */}
+          <label className="form-label-input" htmlFor="form-input-phone">
+            Phone number:
+          </label>
+          <input
+            className="form-input"
+            type="number"
+            name="name"
+            id="form-input-phone"
+            value={this.state.inputPhoneText}
+            onChange={this.handleInputPhoneChange}
+          />
+          {/* textarea: ADRESS */}
           <label className="form-label-textarea" htmlFor="form-textarea">
-            Some info:
+            Adress:
           </label>
           <textarea
             className="form-textarea"
@@ -84,21 +126,34 @@ export class Form extends React.Component<Record<string, never>, FormState> {
             value={this.state.textareaText}
             onChange={this.handleTextareaChange}
           ></textarea>
-          {/* select */}
+          {/* select: DELIVERY */}
           <select value={this.state.selectText} onChange={this.handleSelectChange}>
-            <option value="">--Choose your position--</option>
-            <option value="Front-end developer">Front-end developer</option>
-            <option value="Back-end developer">Back-end developer</option>
+            <option value="">--Delivery method--</option>
+            <option value="self-delivery">Self-delivery</option>
+            <option value="courier delivery">Courier delivery</option>
           </select>
+          {/* checkbox switcher: PAYMENT */}
+          <label className="toggle">
+            <input
+              type="checkbox"
+              value={this.state.checkboxText}
+              onChange={this.handleCheckboxChange}
+            />
+            <span className="slider"></span>
+            <span className="labels" data-on="card" data-off="cash"></span>
+          </label>
           {/* button */}
-          <button className="form-button" onClick={this.handleShow}>
-            Show
+          <button type="submit" className="form-button" onClick={this.handleSubmit}>
+            Submit
           </button>
         </form>
-
-        <h2>{this.state.showData.name}</h2>
-        <h3>{this.state.showData.text}</h3>
-        <h3>{this.state.showData.position}</h3>
+        <div className="form-card card">
+          <p className="card__item">Name: {this.state.showData.name}</p>
+          <p className="card__item">Phone number: {this.state.showData.phone}</p>
+          <p className="card__item">Adress: {this.state.showData.adress}</p>
+          <p className="card__item">Delivery method: {this.state.showData.delivery}</p>
+          <p className="card__item">Payment option: {this.state.showData.payment}</p>
+        </div>
       </>
     );
   }
