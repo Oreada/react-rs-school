0f8bee84ec6dcd54b399064a2f62ef114e7b4ec2
Olga Antushevich <antushevich@gmail.com>
2022-10-08 13:57:31 +0300

    feat: add method addCard and state to FormsPage

--- react-components/src/pages/FormsPage.tsx
+++ react-components/src/pages/FormsPage.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { ReactNode } from 'react';
+import { ICard } from '../components/Card';
 import { Form } from '../components/Form';
 
-export class FormsPage extends React.Component {
+interface FormPageState {
+  cards: Array<ICard>;
+}
+
+export class FormsPage extends React.Component<Record<string, never>, FormPageState> {
+  addCard = (card: ICard) => {
+    this.setState(({ cards }) => {
+      const prevCards = [...cards];
+      return { cards: [...prevCards, card] };
+    });
+  };
+
+  state = {
+    cards: [],
+  };
+
   render(): ReactNode {
     return (
       <div className="form-page">
-        <Form />
+        <Form addCard={this.addCard} cards={this.state.cards} />
       </div>
     );
   }
