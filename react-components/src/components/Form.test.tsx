b738aaedb06e3477903928c1f7b3ff0c27e22987
Olga Antushevich <antushevich@gmail.com>
2022-10-10 22:39:57 +0300

    feat: add validation test for Form

--- react-components/src/components/Form.test.tsx
+++ react-components/src/components/Form.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Form } from './Form';
 import { CARDS } from '../data/cards';
@@ -7,16 +7,26 @@ import { ICard } from './Card';
 const addCardFunc: (card: ICard) => void = jest.fn();
 
 describe('Form component', () => {
-  test('Calls addCardFunc with submit', () => {
+  test('Passes validation and Calls addCard after a submit', () => {
     render(<Form addCard={addCardFunc} cards={CARDS} />);
 
-    (document.getElementById('form-input-name') as HTMLInputElement).value = 'Olga';
-    (document.getElementById('form-input-phone') as HTMLInputElement).value = '777';
-    (document.getElementById('form-textarea') as HTMLTextAreaElement).value = 'Moldova';
-    (document.getElementById('form-select') as HTMLSelectElement).value = 'self-delivery';
-    (document.getElementById('form-checkbox') as HTMLInputElement).checked = true;
+    fireEvent.change(screen.getByTestId('form-input-name'), { target: { value: '1111' } });
+    fireEvent.change(screen.getByTestId('form-input-phone'), { target: { value: '' } });
+    fireEvent.change(screen.getByTestId('form-textarea'), { target: { value: '' } });
+    fireEvent.change(screen.getByTestId('form-select'), { target: { value: '' } });
+    fireEvent.change(screen.getByTestId('form-checkbox'), { target: { checked: true } });
+
     const formButton = screen.getByTestId('form-button');
-    formButton.removeAttribute('disabled');
+    userEvent.click(formButton);
+
+    fireEvent.change(screen.getByTestId('form-input-name'), { target: { value: '' } });
+    userEvent.click(formButton);
+
+    fireEvent.change(screen.getByTestId('form-input-name'), { target: { value: 'Olga' } });
+    fireEvent.change(screen.getByTestId('form-input-phone'), { target: { value: '777' } });
+    fireEvent.change(screen.getByTestId('form-textarea'), { target: { value: 'Moldova' } });
+    fireEvent.change(screen.getByTestId('form-select'), { target: { value: 'self-delivery' } });
+    fireEvent.change(screen.getByTestId('form-checkbox'), { target: { checked: true } });
     userEvent.click(formButton);
 
     expect(addCardFunc).toHaveBeenCalledTimes(1);
@@ -30,7 +40,8 @@ describe('Form component', () => {
 
   test('Input value displays', () => {
     render(<Form addCard={addCardFunc} cards={CARDS} />);
-    (document.getElementById('form-input-name') as HTMLInputElement).value = 'Olga';
+    // (document.getElementById('form-input-name') as HTMLInputElement).value = 'Olga';
+    fireEvent.change(screen.getByTestId('form-input-name'), { target: { value: 'Olga' } });
 
     const inputName = screen.getByDisplayValue('Olga');
 
