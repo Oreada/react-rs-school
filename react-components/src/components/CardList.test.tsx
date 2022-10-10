cab192d17b934b0672d57ff5f47889cc610c5f4b
Olga Antushevich <antushevich@gmail.com>
2022-10-10 18:09:39 +0300

    refactor: change the describe of test

--- react-components/src/components/CardList.test.tsx
+++ react-components/src/components/CardList.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import { CardsList } from './CardsList';
 import { CARDS } from '../data/cards';
 
-describe('CardList component', () => {
-  test('CardList renders', () => {
+describe('CardsList component', () => {
+  test('CardsList renders', () => {
     render(<CardsList data={CARDS} />);
 
     expect(screen.getByTestId('cards-list')).toBeInTheDocument();
