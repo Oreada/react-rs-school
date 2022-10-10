d8ca4312ab3f1b224a92eeffe0933b131b505b38
Olga Antushevich <antushevich@gmail.com>
2022-10-10 16:26:20 +0300

    feat: add validation and form-button disabling

--- react-components/src/components/Form.tsx
+++ react-components/src/components/Form.tsx
@@ -15,7 +15,12 @@ interface FormState {
   adressField: string;
   deliveryField: string;
   paymentField: boolean;
-  errorMessage: string;
+  nameErrorMessage: string;
+  phoneErrorMessage: string;
+  adressErrorMessage: string;
+  deliveryErrorMessage: string;
+  isSubmitted: boolean;
+  isDisabled: boolean;
 }
 
 export class Form extends React.Component<FormProps, FormState> {
@@ -29,7 +34,12 @@ export class Form extends React.Component<FormProps, FormState> {
     adressField: '',
     deliveryField: '',
     paymentField: false,
-    errorMessage: '',
+    nameErrorMessage: '',
+    phoneErrorMessage: '',
+    adressErrorMessage: '',
+    deliveryErrorMessage: '',
+    isSubmitted: false,
+    isDisabled: true,
   };
 
   refNameField: React.RefObject<HTMLInputElement> = React.createRef();
@@ -38,32 +48,103 @@ export class Form extends React.Component<FormProps, FormState> {
   refDeliveryField: React.RefObject<HTMLSelectElement> = React.createRef();
   refPaymentField: React.RefObject<HTMLInputElement> = React.createRef();
 
+  handleChange = (): void => {
+    this.setState({
+      isDisabled: false,
+    });
+  };
+
   validateFields = () => {
-    // let isValid = true;
+    let isValid = true;
 
     if (!this.state.nameField.length) {
-      // isValid = false;
+      isValid = false;
       this.setState({
-        errorMessage: 'Enter your name',
+        nameErrorMessage: 'Enter your name',
       });
-      console.log('Enter your name');
+    } else if (!/^[ a-zA-Zа-яА-Я]+$/.test(this.state.nameField)) {
+      isValid = false;
+      this.setState({
+        nameErrorMessage: 'The name must contain only letters',
+      });
     } else {
       this.setState({
-        errorMessage: '',
+        nameErrorMessage: '',
       });
     }
+
+    if (!this.state.phoneField.length) {
+      isValid = false;
+      this.setState({
+        phoneErrorMessage: 'Enter your phone number',
+      });
+    } else {
+      this.setState({
+        phoneErrorMessage: '',
+      });
+    }
+
+    if (!this.state.adressField.length) {
+      isValid = false;
+      this.setState({
+        adressErrorMessage: 'Enter your adress',
+      });
+    } else {
+      this.setState({
+        adressErrorMessage: '',
+      });
+    }
+
+    if (!this.state.deliveryField) {
+      isValid = false;
+      this.setState({
+        deliveryErrorMessage: 'Choose a delivery method',
+      });
+    } else {
+      this.setState({
+        deliveryErrorMessage: '',
+      });
+    }
+
+    this.setState({
+      isDisabled: !isValid,
+    });
+
+    return isValid;
   };
 
   onSubmit = (): void => {
-    this.validateFields();
+    const isValid = this.validateFields();
+    console.log(isValid);
 
-    this.props.addCard({
-      name: this.state.nameField,
-      phone: this.state.phoneField,
-      adress: this.state.adressField,
-      delivery: this.state.deliveryField,
-      payment: this.state.paymentField ? 'card' : 'cash',
-    });
+    if (isValid) {
+      this.setState({
+        isSubmitted: true,
+        isDisabled: true,
+      });
+
+      this.props.addCard({
+        name: this.state.nameField,
+        phone: this.state.phoneField,
+        adress: this.state.adressField,
+        delivery: this.state.deliveryField,
+        payment: this.state.paymentField ? 'card' : 'cash',
+      });
+
+      (this.refNameField.current as HTMLInputElement).value = '';
+      (this.refPhoneField.current as HTMLInputElement).value = '';
+      (this.refAdressField.current as HTMLTextAreaElement).value = '';
+      (this.refDeliveryField.current as HTMLSelectElement).value = '';
+      (this.refPaymentField.current as HTMLInputElement).checked = false;
+
+      setTimeout(() => {
+        this.setState({
+          isSubmitted: false,
+        });
+      }, 3000);
+    } else {
+      console.log('Enter the valid data into fields');
+    }
   };
 
   handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -83,7 +164,7 @@ export class Form extends React.Component<FormProps, FormState> {
   render(): ReactNode {
     return (
       <>
-        <form className="form" onSubmit={this.handleSubmit}>
+        <form className="form" onSubmit={this.handleSubmit} onChange={this.handleChange}>
           <label className="form-label-input" htmlFor="form-input-name">
             Name:
           </label>
@@ -94,10 +175,9 @@ export class Form extends React.Component<FormProps, FormState> {
             id="form-input-name"
             ref={this.refNameField}
           />
-          {this.state.errorMessage && <ErrorMessage errorMessage={this.state.errorMessage} />}
-          {/* <ErrorMessage errorMessage={'my test message'} /> */}
-          {/* {this.state.errorMessage} */}
-
+          {this.state.nameErrorMessage && (
+            <ErrorMessage errorMessage={this.state.nameErrorMessage} />
+          )}
           <label className="form-label-input" htmlFor="form-input-phone">
             Phone number:
           </label>
@@ -108,7 +188,9 @@ export class Form extends React.Component<FormProps, FormState> {
             id="form-input-phone"
             ref={this.refPhoneField}
           />
-
+          {this.state.phoneErrorMessage && (
+            <ErrorMessage errorMessage={this.state.phoneErrorMessage} />
+          )}
           <label className="form-label-textarea" htmlFor="form-textarea">
             Adress:
           </label>
@@ -118,22 +200,43 @@ export class Form extends React.Component<FormProps, FormState> {
             id="form-textarea"
             ref={this.refAdressField}
           ></textarea>
-
+          {this.state.adressErrorMessage && (
+            <ErrorMessage errorMessage={this.state.adressErrorMessage} />
+          )}
           <select className="form-select" ref={this.refDeliveryField}>
             <option value="">--Delivery method--</option>
             <option value="self-delivery">Self-delivery</option>
             <option value="courier delivery">Courier delivery</option>
           </select>
-
+          {this.state.deliveryErrorMessage && (
+            <ErrorMessage errorMessage={this.state.deliveryErrorMessage} />
+          )}
           <label className="toggle">
             <input type="checkbox" ref={this.refPaymentField} />
             <span className="slider"></span>
             <span className="labels" data-on="card" data-off="cash"></span>
           </label>
-
-          <button type="submit" className="form-button" value="Submit">
+          <button
+            type="submit"
+            className="form-button"
+            value="Submit"
+            disabled={this.state.isDisabled}
+          >
             Submit
           </button>
+          <div
+            className="form-saved"
+            style={this.state.isSubmitted ? { visibility: 'visible' } : { visibility: 'hidden' }}
+          >
+            <img
+              className="form-saved__icon"
+              src="./images/checked-icon.svg"
+              width="20px"
+              height="20px"
+              alt="Checked"
+            />
+            <p className="form-saved__text">The data was successfully saved</p>
+          </div>
         </form>
         <CardsList data={this.props.cards} />
       </>
