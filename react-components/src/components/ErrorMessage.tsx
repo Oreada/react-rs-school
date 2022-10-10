58c603593cbf9b1d061413180f1daa7fb7949113
Olga Antushevich <antushevich@gmail.com>
2022-10-10 18:18:19 +0300

    feat: add tests for ErrorMessage

--- react-components/src/components/ErrorMessage.tsx
+++ react-components/src/components/ErrorMessage.tsx
@@ -6,6 +6,10 @@ interface ErrorMessageProps {
 
 export class ErrorMessage extends React.Component<ErrorMessageProps> {
   render(): ReactNode {
-    return <div className="text-center text-xl text-red-800">{this.props.errorMessage}</div>;
+    return (
+      <div className="text-center text-xl text-red-800" data-testid="error-message">
+        {this.props.errorMessage}
+      </div>
+    );
   }
 }
