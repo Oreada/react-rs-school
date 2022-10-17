f35d1af629f27d9f02ffb1555d91852e742f61b5
Olga Antushevich <antushevich@gmail.com>
2022-10-17 11:06:42 +0300

    feat: add tests for AboutPage

--- react-components/src/pages/AboutPage.tsx
+++ react-components/src/pages/AboutPage.tsx
@@ -3,6 +3,10 @@ import { ReactNode } from 'react';
 
 export class AboutPage extends React.Component {
   render(): ReactNode {
-    return <main className="about-page">This is the About page</main>;
+    return (
+      <main className="about-page" data-testid="about-page">
+        This is the About page
+      </main>
+    );
   }
 }
