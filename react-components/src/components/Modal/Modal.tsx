6dddcd2a154992d08edac4c398e5411f038c35b2
Olga Antushevich <antushevich@gmail.com>
2022-10-17 20:03:45 +0300

    refactor: add Modal.module.css

--- react-components/src/components/Modal/Modal.tsx
+++ react-components/src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import styles from './Modal.module.css';
 
 interface ModalProps {
   children: React.ReactNode;
@@ -15,8 +16,8 @@ export class Modal extends React.Component<ModalProps> {
   render(): ReactNode {
     return (
       <>
-        <div className="modal__overlay" onClick={this.clickHandler}></div>
-        <div className="modal__main" data-testid="modal-main">
+        <div className={styles['modal__overlay']} onClick={this.clickHandler}></div>
+        <div className={styles['modal__main']} data-testid="modal-main">
           {/* <h1 className="text-center mb-2 text-2xl">{this.props.title}</h1> */}
 
           {/* //! тут будет отображаться то содержимое, которое вставлено внутрь <Modal></Modal>: */}
