4dd724d1a80c2fab0b0ae59324802b9c7b1c1213
Olga Antushevich <antushevich@gmail.com>
2022-10-17 16:14:21 +0300

    refactor: relocate function getDetails to module

--- react-components/src/components/ArtWork.tsx
+++ react-components/src/components/ArtWork.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { IDetailsData } from './ArtWorksList';
 import { getImagePath } from '../api/helpers';
+import { getDetails } from '../api/getDetails';
 
 export interface IArtWork {
   id: number;
@@ -17,32 +18,18 @@ export class ArtWork extends React.Component<IArtWork> {
     super(props);
   }
 
-  getDetails = async (idArtWork: number) => {
-    try {
-      this.props.forDetails(null, true);
-      const response = await fetch(
-        `https://api.artic.edu/api/v1/artworks/${idArtWork}?query[term][is_public_domain]=true&fields=id,title,artist_title,date_display,artwork_type_title,dimensions,artist_display,image_id&page=1&limit=20`
-      );
-      if (!response.ok) {
-        throw new Error(`Request failed with status code ${response.status}`);
-      }
-      const respJson = await response.json();
-      if (!respJson.hasOwnProperty('data')) {
-        throw new Error(`Incorrect content format`);
-      }
-      const artWork = respJson.data;
-      this.props.forDetails(artWork, false);
-      console.log('ArtWork.getDetails.artWork', artWork);
-    } catch (e: unknown) {
-      const err = e as Error;
-      console.log(err.message);
-    }
-  };
-
-  clickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+  clickHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
     this.props.onClick(true);
     const id = Number((event.target as HTMLButtonElement).dataset.id);
-    this.getDetails(id);
+
+    try {
+      this.props.forDetails(null, true);
+      const artWorkDetails = await getDetails(id);
+      this.props.forDetails(artWorkDetails as IDetailsData, false);
+    } catch (e: unknown) {
+      const err = e as Error;
+      console.log(err.message); //! тут не вывожу сообщение об ошибке пользователю
+    }
   };
 
   render(): ReactNode {
