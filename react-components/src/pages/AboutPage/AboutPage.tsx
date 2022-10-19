97cf7276a9d6657991e0b4c5e82d66bdb0f80799
Olga Antushevich <antushevich@gmail.com>
2022-10-19 14:27:03 +0300

    refactor: rewrite AboutPage with function

--- react-components/src/pages/AboutPage/AboutPage.tsx
+++ react-components/src/pages/AboutPage/AboutPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { ReactNode } from 'react';
 
-export class AboutPage extends React.Component {
-  render(): ReactNode {
-    return (
-      <main className="about-page" data-testid="about-page">
-        This is the About page
-      </main>
-    );
-  }
+export function AboutPage() {
+  return (
+    <main className="about-page" data-testid="about-page">
+      This is the About page
+    </main>
+  );
 }
