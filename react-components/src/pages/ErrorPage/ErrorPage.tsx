94fcfcf0a94771aaf5a1cd812c50274f01fe0ddc
Olga Antushevich <antushevich@gmail.com>
2022-10-19 14:45:08 +0300

    refactor: rewrite ErrorPage with function

--- react-components/src/pages/ErrorPage/ErrorPage.tsx
+++ react-components/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { ReactNode } from 'react';
 
-export class ErrorPage extends React.Component {
-  render(): ReactNode {
-    return (
-      <main className="error-page" data-testid="error-page">
-        Error 404. Page was not found
-      </main>
-    );
-  }
+export function ErrorPage() {
+  return (
+    <main className="error-page" data-testid="error-page">
+      Error 404. Page was not found
+    </main>
+  );
 }
