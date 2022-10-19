3ac80997056d0f5fbb32b0f84ddeb564f36941b5
Olga Antushevich <antushevich@gmail.com>
2022-10-19 18:14:57 +0300

    refactor: rewrite Card with function

--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styles from './Card.module.css';
 
 export interface ICard {
@@ -9,20 +9,14 @@ export interface ICard {
   payment: string;
 }
 
-export class Card extends React.Component<ICard> {
-  constructor(props: ICard) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <div className={styles['card']} data-testid="card">
-        <p className={styles['card__item']}>Name: {this.props.name}</p>
-        <p className={styles['card__item']}>Phone number: {this.props.phone}</p>
-        <p className={styles['card__item']}>Adress: {this.props.adress}</p>
-        <p className={styles['card__item']}>Delivery method: {this.props.delivery}</p>
-        <p className={styles['card__item']}>Payment option: {this.props.payment}</p>
-      </div>
-    );
-  }
+export function Card(props: ICard) {
+  return (
+    <div className={styles['card']} data-testid="card">
+      <p className={styles['card__item']}>Name: {props.name}</p>
+      <p className={styles['card__item']}>Phone number: {props.phone}</p>
+      <p className={styles['card__item']}>Adress: {props.adress}</p>
+      <p className={styles['card__item']}>Delivery method: {props.delivery}</p>
+      <p className={styles['card__item']}>Payment option: {props.payment}</p>
+    </div>
+  );
 }
