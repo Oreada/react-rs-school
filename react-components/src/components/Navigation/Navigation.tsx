7cbf03e9c4354ba1c039f409c7067a4f96f84edf
Olga Antushevich <antushevich@gmail.com>
2022-10-19 18:27:21 +0300

    refactor: rewrite Navigation with function

--- react-components/src/components/Navigation/Navigation.tsx
+++ react-components/src/components/Navigation/Navigation.tsx
@@ -1,34 +1,32 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
-export class Navigation extends React.Component {
-  render(): ReactNode {
-    return (
-      <nav className={styles.navigation} data-testid="navigation">
-        <span className={styles.navigation__title}>Art Institute of Chicago</span>
-        <span className={styles.navigation__links}>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
-            end
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
-          >
-            About
-          </NavLink>
-          <NavLink
-            to="/forms"
-            className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
-          >
-            Forms
-          </NavLink>
-        </span>
-      </nav>
-    );
-  }
+export function Navigation() {
+  return (
+    <nav className={styles.navigation} data-testid="navigation">
+      <span className={styles.navigation__title}>Art Institute of Chicago</span>
+      <span className={styles.navigation__links}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/forms"
+          className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
+        >
+          Forms
+        </NavLink>
+      </span>
+    </nav>
+  );
 }
