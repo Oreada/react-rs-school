35a2bb4a23e2e24f4114998ccbeda8c67f7e7a73
Olga Antushevich <antushevich@gmail.com>
2022-10-19 17:35:14 +0300

    refactor: rewrite ArtWork with function

--- react-components/src/components/ArtWork/ArtWork.tsx
+++ react-components/src/components/ArtWork/ArtWork.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { IDetailsData } from '../ArtWorksList/ArtWorksList';
 import { getImagePath } from '../../api/helpers';
 import { getDetails } from '../../api/getDetails';
@@ -14,49 +14,43 @@ export interface IArtWork {
   forDetails: (dataDetails: IDetailsData | null, loadingDetails: boolean) => void;
 }
 
-export class ArtWork extends React.Component<IArtWork> {
-  constructor(props: IArtWork) {
-    super(props);
-  }
-
-  clickHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    this.props.onClick(true);
+export function ArtWork(props: IArtWork) {
+  const clickHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    props.onClick(true);
     const id = Number((event.target as HTMLButtonElement).dataset.id);
 
     try {
-      this.props.forDetails(null, true);
+      props.forDetails(null, true);
       const artWorkDetails = await getDetails(id);
-      this.props.forDetails(artWorkDetails as IDetailsData, false);
+      props.forDetails(artWorkDetails as IDetailsData, false);
     } catch (e: unknown) {
       const err = e as Error;
       console.log(err.message); //! тут не вывожу сообщение об ошибке пользователю
     }
   };
 
-  render(): ReactNode {
-    const imagePath = getImagePath(this.props.image_id);
+  const imagePath = getImagePath(props.image_id);
 
-    return (
-      <li className={styles['artwork']} data-testid="artwork">
-        <div className={styles['artwork__box']}>
-          <div className={styles['artwork__picture']}>
-            <img src={imagePath} alt="ArtWork" className={styles['artwork__image']} />
-          </div>
-          <div className={styles['artwork__info']}>
-            <div className={styles['info__item_title']}>&quot;{this.props.title}&quot;</div>
-            <div className={styles['info__item_artist']}>{this.props.artist_title}</div>
-            <div className={styles['info__item_date']}>{this.props.date_display}</div>
-          </div>
-          <button
-            className={styles['artwork__button']}
-            onClick={this.clickHandler}
-            data-id={this.props.id}
-            data-testid="artwork-button"
-          >
-            Read more
-          </button>
+  return (
+    <li className={styles['artwork']} data-testid="artwork">
+      <div className={styles['artwork__box']}>
+        <div className={styles['artwork__picture']}>
+          <img src={imagePath} alt="ArtWork" className={styles['artwork__image']} />
         </div>
-      </li>
-    );
-  }
+        <div className={styles['artwork__info']}>
+          <div className={styles['info__item_title']}>&quot;{props.title}&quot;</div>
+          <div className={styles['info__item_artist']}>{props.artist_title}</div>
+          <div className={styles['info__item_date']}>{props.date_display}</div>
+        </div>
+        <button
+          className={styles['artwork__button']}
+          onClick={clickHandler}
+          data-id={props.id}
+          data-testid="artwork-button"
+        >
+          Read more
+        </button>
+      </div>
+    </li>
+  );
 }
