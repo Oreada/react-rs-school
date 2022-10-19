67ab64d578c72342c1c586e4d40a66748a973263
Olga Antushevich <antushevich@gmail.com>
2022-10-19 18:17:53 +0300

    refactor: rewrite CardsList with function

--- react-components/src/components/CardsList/CardsList.tsx
+++ react-components/src/components/CardsList/CardsList.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { ICard, Card } from '../Card/Card';
 import styles from './CardsList.module.css';
 
@@ -6,25 +6,19 @@ interface CardsListProps {
   data: Array<ICard>;
 }
 
-export class CardsList extends React.Component<CardsListProps> {
-  constructor(props: CardsListProps) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <div className={styles['cards__list']} data-testid="cards-list">
-        {this.props.data.map((card: ICard, index: number) => (
-          <Card
-            key={index}
-            name={card.name}
-            phone={card.phone}
-            adress={card.adress}
-            delivery={card.delivery}
-            payment={card.payment}
-          />
-        ))}
-      </div>
-    );
-  }
+export function CardsList(props: CardsListProps) {
+  return (
+    <div className={styles['cards__list']} data-testid="cards-list">
+      {props.data.map((card: ICard, index: number) => (
+        <Card
+          key={index}
+          name={card.name}
+          phone={card.phone}
+          adress={card.adress}
+          delivery={card.delivery}
+          payment={card.payment}
+        />
+      ))}
+    </div>
+  );
 }
