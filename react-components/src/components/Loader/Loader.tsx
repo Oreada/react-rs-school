2a1064194c9aa65868893835d89daa8fcca3f899
Olga Antushevich <antushevich@gmail.com>
2022-10-19 18:34:24 +0300

    refactor: rewrite Loader with function

--- react-components/src/components/Loader/Loader.tsx
+++ react-components/src/components/Loader/Loader.tsx
@@ -1,12 +1,10 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styles from './Loader.module.css';
 
-export class Loader extends React.Component {
-  render(): ReactNode {
-    return (
-      <div className={styles['preload']}>
-        <div className={styles['preload__bg']}></div>
-      </div>
-    );
-  }
+export function Loader() {
+  return (
+    <div className={styles['preload']}>
+      <div className={styles['preload__bg']}></div>
+    </div>
+  );
 }
