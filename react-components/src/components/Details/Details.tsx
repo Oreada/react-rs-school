8901e6feef5f9a3aa72a86fb1fa02c7c05e5ac4d
Olga Antushevich <antushevich@gmail.com>
2022-10-19 18:20:45 +0300

    refactor: rewrite Details with function

--- react-components/src/components/Details/Details.tsx
+++ react-components/src/components/Details/Details.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { getImagePath } from '../../api/helpers';
 import { IDetailsData } from '../ArtWorksList/ArtWorksList';
 import styles from './Details.module.css';
@@ -7,40 +7,30 @@ export interface IDetails {
   data: IDetailsData | null;
 }
 
-export class Details extends React.Component<IDetails> {
-  render(): ReactNode {
-    const imagePath = getImagePath(this.props.data?.image_id as string);
+export function Details(props: IDetails) {
+  const imagePath = getImagePath(props.data?.image_id as string);
 
-    return (
-      <div className={styles['details']} data-testid="details">
-        <div className={styles['details__box']}>
-          <div className={styles['details__picture']}>
-            <img src={imagePath} alt="ArtWork" className={styles['details__image']} />
-          </div>
-          <div className={styles['details__info']}>
-            <div className={styles['details__header']}>Title of the artwork:</div>
-            <div className={styles['details__item']}>
-              &quot;{this.props.data?.title as string}&quot;
-            </div>
-            <div className={styles['details__header']}>Name of the author: </div>
-            <div className={styles['details__item_artist']}>
-              {this.props.data?.artist_title as string}
-            </div>
-            <div className={styles['details__header']}>Time of creation:</div>
-            <div className={styles['details__item']}>{this.props.data?.date_display as string}</div>
-            <div className={styles['details__header']}>The kind of work:</div>
-            <div className={styles['details__item']}>
-              {this.props.data?.artwork_type_title as string}
-            </div>
-            <div className={styles['details__header']}>Dimensions of the work:</div>
-            <div className={styles['details__item']}>{this.props.data?.dimensions as string}</div>
-            <div className={styles['details__header']}>Author&apos;s description:</div>
-            <div className={styles['details__item']}>
-              {this.props.data?.artist_display as string}
-            </div>
-          </div>
+  return (
+    <div className={styles['details']} data-testid="details">
+      <div className={styles['details__box']}>
+        <div className={styles['details__picture']}>
+          <img src={imagePath} alt="ArtWork" className={styles['details__image']} />
+        </div>
+        <div className={styles['details__info']}>
+          <div className={styles['details__header']}>Title of the artwork:</div>
+          <div className={styles['details__item']}>&quot;{props.data?.title as string}&quot;</div>
+          <div className={styles['details__header']}>Name of the author: </div>
+          <div className={styles['details__item_artist']}>{props.data?.artist_title as string}</div>
+          <div className={styles['details__header']}>Time of creation:</div>
+          <div className={styles['details__item']}>{props.data?.date_display as string}</div>
+          <div className={styles['details__header']}>The kind of work:</div>
+          <div className={styles['details__item']}>{props.data?.artwork_type_title as string}</div>
+          <div className={styles['details__header']}>Dimensions of the work:</div>
+          <div className={styles['details__item']}>{props.data?.dimensions as string}</div>
+          <div className={styles['details__header']}>Author&apos;s description:</div>
+          <div className={styles['details__item']}>{props.data?.artist_display as string}</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
