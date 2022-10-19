bac42b05e6d886f6e897def9b336e8e0578c5708
Olga Antushevich <antushevich@gmail.com>
2022-10-19 18:23:25 +0300

    refactor: rewrite ErrorMessage with function

--- react-components/src/components/ErrorMessage/ErrorMessage.tsx
+++ react-components/src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,16 +1,14 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styles from './ErrorMessage.module.css';
 
 interface ErrorMessageProps {
   errorMessage: string;
 }
 
-export class ErrorMessage extends React.Component<ErrorMessageProps> {
-  render(): ReactNode {
-    return (
-      <div className={styles['error-message']} data-testid="error-message">
-        {this.props.errorMessage}
-      </div>
-    );
-  }
+export function ErrorMessage(props: ErrorMessageProps) {
+  return (
+    <div className={styles['error-message']} data-testid="error-message">
+      {props.errorMessage}
+    </div>
+  );
 }
