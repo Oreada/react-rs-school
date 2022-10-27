bbfebc00b59808c76a57e2fa7bb8dc73c7906d03
Olga Antushevich <antushevich@gmail.com>
2022-10-27 11:57:08 +0300

    refactor: change query in url

--- react-components/src/api/getData.tsx
+++ react-components/src/api/getData.tsx
@@ -3,7 +3,7 @@ import { IArtWorkData } from '../pages/HomePage/HomePage';
 export const getData = async (): Promise<Array<IArtWorkData> | undefined> => {
   try {
     const response = await fetch(
-      `https://api.artic.edu/api/v1/artworks/search?q=painting&query[term][is_public_domain]=true&fields=id,title,artist_title,date_display,image_id&page=1&limit=10`
+      `https://api.artic.edu/api/v1/artworks/search?q=landscape&query[term][is_public_domain]=true&fields=id,title,artist_title,date_display,image_id&page=1&limit=9`
     );
     if (!response.ok) {
       throw new Error(`Request failed with status code ${response.status}`);
