586fbfda97bad31a020215366c565926a87af4d8
Olga Antushevich <antushevich@gmail.com>
2022-10-30 14:10:16 +0200

    feat: add IGetData interface, change the returning values

--- react-components/src/api/getSortedData.tsx
+++ react-components/src/api/getSortedData.tsx
@@ -1,43 +1,10 @@
 import { IArtWorkData } from '../pages/HomePage/HomePage';
 
-export const getSortedData = async (
-  value: string,
-  limit: string,
-  obj: DateSorting | TitleSorting | AuthorSorting | unknown
-): Promise<Array<IArtWorkData> | undefined> => {
-  console.log('value=', value);
-  console.log('limit=', limit);
-  console.log('obj=', obj);
-  try {
-    const response = await fetch(
-      `https://api.artic.edu/api/v1/artworks/search?q=${value}&query[term][is_public_domain]=true&fields=id,title,artist_title,date_display,image_id&page=1&limit=${limit}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(obj),
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(`Request failed with status code ${response.status}`);
-    }
-    const respJson = await response.json();
-    if (!respJson.hasOwnProperty('data')) {
-      throw new Error(`Incorrect content format`);
-    }
-    const artWorksList = respJson.data;
-    console.log('artWorksList', artWorksList);
-
-    return artWorksList;
-  } catch (e: unknown) {
-    const err = e as Error;
-    console.log(err.message);
-
-    throw new Error(`Error: ${err.message}`);
-  }
-};
+export interface IGetData {
+  artWorksList: Array<IArtWorkData>;
+  totalPages: string;
+  currentPage: string;
+}
 
 export interface DateSorting {
   sort: [{ date_start: string }];
@@ -74,3 +41,51 @@ export interface AuthorSorting {
   };
   sort: [{ 'artist_title.keyword': string }];
 }
+
+export const getSortedData = async (
+  value: string,
+  limit: string,
+  obj: DateSorting | TitleSorting | AuthorSorting | unknown,
+  page: string
+): Promise<IGetData | undefined> => {
+  console.log('value=', value);
+  console.log('limit=', limit);
+  console.log('obj=', obj);
+  console.log('page=', page);
+  try {
+    const response = await fetch(
+      `https://api.artic.edu/api/v1/artworks/search?q=${value}&query[term][is_public_domain]=true&fields=id,title,artist_title,date_display,image_id&page=${page}&limit=${limit}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status code ${response.status}`);
+    }
+    const respJson = await response.json();
+    if (!respJson.hasOwnProperty('data')) {
+      throw new Error(`Incorrect content format`);
+    }
+    const artWorksList = respJson.data;
+    console.log('artWorksList', artWorksList);
+
+    const totalPages = respJson.pagination.total_pages;
+    const currentPage = respJson.pagination.current_page;
+
+    return {
+      artWorksList: artWorksList,
+      totalPages: totalPages,
+      currentPage: currentPage,
+    };
+  } catch (e: unknown) {
+    const err = e as Error;
+    console.log(err.message);
+
+    throw new Error(`Error: ${err.message}`);
+  }
+};
